e9d278928a3c1b6ffc82cdaeac81db170ff06316
Sam  Shenoi <samshenoi@sams-mbp.attlocal.net>
2017-12-20 10:07:34 -0600

    Hello

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,5 @@
 
-var elementUsed = document.getElementById("headerA");
-elementUsed.addEventListener("click",displayDrop);
 
-function displayDrop()
-{
-  var dropDownMenu = document.getElementById("dd");
-  var className = dropDownMenu.className;
-
-  if(dropDownMenu.className == "appear")
-  {
-    dropDownMenu.className = "hide";
-  }
-  else if(dropDownMenu.className == "hide") {
-     dropDownMenu.className = "appear";
-   }
-
-}
 var sendMessageButton = document.getElementById("send");
 sendMessageButton.addEventListener("click",sendMessage)
 
